171d054ffb5d2023a46cb7f16f61aa41cab9f410
ShubhamPaliwal157 <shubhampaliwal157@gmail.com>
2025-01-25 21:21:44 +0530

    some changes to theme toggle

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//navbar style after scroll
 document.addEventListener("DOMContentLoaded", () => {
     const navbar = document.querySelector("nav");
   
@@ -6,6 +7,28 @@ document.addEventListener("DOMContentLoaded", () => {
         navbar.classList.add("scrolled");
       } else {
         navbar.classList.remove("scrolled");
+      }
+    });
+  });
+
+  //dark-light theme
+  document.addEventListener("DOMContentLoaded", () => {
+    const themeToggleButton = document.getElementById("themeBtn");
+  
+    // Check for saved theme in localStorage
+    const savedTheme = localStorage.getItem("theme");
+    if (savedTheme === "dark") {
+      document.body.classList.add("dark-mode");
+    }
+  
+    themeToggleButton.addEventListener("click", () => {
+      document.body.classList.toggle("dark-mode");
+  
+      // Save the theme preference in localStorage
+      if (document.body.classList.contains("dark-mode")) {
+        localStorage.setItem("theme", "dark");
+      } else {
+        localStorage.setItem("theme", "light");
       }
     });
   });
